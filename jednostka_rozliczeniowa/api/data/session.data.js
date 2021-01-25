ca497d3a51857055f75f84e9ad4663e5ed89f754
Marcin Wielgos <wielgos.me@gmail.com>
2021-01-25 19:50:10 +0100

    Update session.data.js

--- jednostka_rozliczeniowa/api/data/session.data.js
+++ jednostka_rozliczeniowa/api/data/session.data.js
@@ -88,7 +88,7 @@ exports.lastlyServedSession = () => {
     return lastlyServedSession
   } else {
     let closestSession = sessionsConf.find(s => {
-      return Date.parse(`01/01/1970/ ${s.hourAnnounce}:00`) > Date.parse(`01/01/1970/ ${operatingDate.getHours()}:${operatingDate.getMinutes()}:00`)
+      return Date.parse(`01/01/1970/ ${s.hourAnnounce}:00`) < Date.parse(`01/01/1970/ ${operatingDate.getHours()}:${operatingDate.getMinutes()}:00`)
     })
 
     lastlyServedSession += operatingDate.getFullYear()
