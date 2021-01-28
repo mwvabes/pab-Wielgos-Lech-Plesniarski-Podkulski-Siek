76a3b16102c2275b61ac5169f853284e2e5d04c1
Marcin Wielgos <wielgos.me@gmail.com>
2021-01-28 22:59:39 +0100

    Update auth.route.js

--- jednostka_rozliczeniowa/api/routes/auth.route.js
+++ jednostka_rozliczeniowa/api/routes/auth.route.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   const router = require("express").Router()
   const authController = require("../controllers/auth.controller.js")
 
-  router.get('/register', authController.register)
+  router.post('/register', authController.register)
 
   app.use('/api/auth', router)
 
