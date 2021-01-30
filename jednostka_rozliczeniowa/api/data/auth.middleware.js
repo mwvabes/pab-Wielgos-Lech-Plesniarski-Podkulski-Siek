0bd7a6d21e669c234e17b85f27664556fad6417a
Marcin Wielgos <wielgos.me@gmail.com>
2021-01-30 22:34:42 +0100

    Update auth.middleware.js

--- jednostka_rozliczeniowa/api/data/auth.middleware.js
+++ jednostka_rozliczeniowa/api/data/auth.middleware.js
@@ -1,7 +1,23 @@
 const passport = require('passport')
-const passportConf = require('./../conf/passport')
 
 
-exports.auth = (request, result, next) => {
-  return passport.authenticate('jwt', { session: false })(request, result, next)
+// exports.auth1 = (request, result, next) => {
+//   return passport.authenticate('jwt', { session: false })(request, result, next)
+// }
+
+exports.auth = (request, response, next) => {
+  passport.authenticate('jwt', { session: false, }, async (error, token) => {
+      if (error || !token) {
+          response.status(401).json({ message: 'Unauthorized' });
+      } 
+      try {
+          const user = await User.findOne({
+              where: { id: token.id },
+          });
+          request.user = user;
+      } catch (error) {
+          next(error);
+      }
+      next();
+  })(request, response, next);   
 }
