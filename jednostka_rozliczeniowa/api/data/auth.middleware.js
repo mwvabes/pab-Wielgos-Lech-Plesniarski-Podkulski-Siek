7372d3ff5424774be9ebe381697ee284c0f54a5d
Marcin Wielgos <wielgos.me@gmail.com>
2021-01-30 20:59:52 +0100

    Update auth.middleware.js

--- jednostka_rozliczeniowa/api/data/auth.middleware.js
+++ jednostka_rozliczeniowa/api/data/auth.middleware.js
@@ -1,5 +1,8 @@
 const passport = require('passport')
+const passportConf = require('./conf/passport')
+
 
 exports.auth = (request, result, next) => {
+  passportConf.passport()
   return passport.authenticate('jwt', { session: false })(request, result, next)
 }
