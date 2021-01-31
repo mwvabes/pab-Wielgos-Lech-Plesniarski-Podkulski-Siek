59edb4fdbc71a9a7953b7766650754980b7ca324
Marcin Wielgos <wielgos.me@gmail.com>
2021-01-31 17:31:46 +0100

    Revert "Update auth.route.js"
    This reverts commit a25ed0915c2b72fc925952dd43901c5c2e448e8f.

--- jednostka_rozliczeniowa/api/routes/auth.route.js
+++ jednostka_rozliczeniowa/api/routes/auth.route.js
@@ -3,7 +3,7 @@ module.exports = (app) => {
   const authController = require("../controllers/auth.controller.js")
   const passport = require("passport")
 
-  router.post('/login', passport.authenticate('local', { session: false }), authController.login)
+  router.post('/login', passport.authenticate('local', {session: false}), authController.login)
   router.post('/register', authController.register)
 
   app.use('/api/auth', router)
