59c0ca19a6c5ca16391ba22d3f736d200cdddc41
Marcin Wielgos <wielgos.me@gmail.com>
2021-01-31 19:43:11 +0100

    Update passport.js

--- jednostka_rozliczeniowa/api/conf/passport.js
+++ jednostka_rozliczeniowa/api/conf/passport.js
@@ -4,30 +4,37 @@ const LocalStrategy = require('passport-local').Strategy;
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+const { secret } = require('./keys');
+
+const UserModel = require('./models/user');
+
 const models = require("../models");
 const User = models.user;
 
 
 exports.passportInit = () => {
   //passport.use(new LocalStrategy(User.authenticate()));
-  // passport.use(User.createStrategy());
+  passport.use(User.createStrategy());
 
-  passport.use(new LocalStrategy({
-    usernameField: username,
-    passwordField: password,
-  }, async (username, password, done) => {
-    try {
-      const userDocument = await UserModel.findOne({username: username}).exec();
-      const passwordsMatch = await bcrypt.compare(password, userDocument.passwordHash);
+  // passport.use(new LocalStrategy({
+  //   usernameField: username,
+  //   passwordField: password,
+  // }, async (username, password, done) => {
+  //   try {
+  //     const userDocument = await User.findOne({username: username}).exec();
+  //     const passwordsMatch = await bcrypt.compare(password, userDocument.passwordHash);
   
-      if (passwordsMatch) {
-        return done(null, userDocument);
-      } else {
-        return done('Incorrect Username / Password');
-      }
-    } catch (error) {
-      done(error);
-    }
-  }));
+  //     if (passwordsMatch) {
+  //       return done(null, userDocument);
+  //     } else {
+  //       return done('Incorrect Username / Password');
+  //     }
+  //   } catch (error) {
+  //     done(error);
+  //   }
+  // }));
 
 }
