dd71c42b78fea4586f78039510c228489f982f8e
Marcin Wielgos <wielgos.me@gmail.com>
2021-01-31 19:24:55 +0100

    Update passport.js

--- jednostka_rozliczeniowa/api/conf/passport.js
+++ jednostka_rozliczeniowa/api/conf/passport.js
@@ -9,18 +9,5 @@ const User = models.user;
 
 
 exports.passportInit = () => {
-  passport.use(new LocalStrategy(
-    function(username, password, done) {
-      User.findOne({ username: username }, function (err, user) {
-        if (err) { return done(err); }
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (!user.validPassword(password)) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, user);
-      });
-    }
-  ));
+  passport.use(new LocalStrategy(User.authenticate()));
 }
