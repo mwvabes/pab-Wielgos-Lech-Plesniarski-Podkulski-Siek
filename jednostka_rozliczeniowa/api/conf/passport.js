739583b8f1d05a3a51d3c5ce4a1c47d65a7674c8
Marcin Wielgos <wielgos.me@gmail.com>
2021-01-31 17:14:23 +0100

    Update passport.js

--- jednostka_rozliczeniowa/api/conf/passport.js
+++ jednostka_rozliczeniowa/api/conf/passport.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const passportJWT = require("passport-jwt");
+const LocalStrategy = passport.Strategy;
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 
