33abc62d87b8de6f5f70c11f941f3a09748613e2
Marcin Wielgos <wielgos.me@gmail.com>
2021-01-31 19:21:01 +0100

    Update passport.js

--- jednostka_rozliczeniowa/api/conf/passport.js
+++ jednostka_rozliczeniowa/api/conf/passport.js
@@ -1,9 +1,25 @@
 const passport = require("passport");
+const passportJWT = require("passport-jwt");
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
 const models = require("../models");
 const User = models.user;
 
 
 exports.passportInit = () => {
-  passport.use(User.createStrategy());
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      });
+    }
+  ));
 }
