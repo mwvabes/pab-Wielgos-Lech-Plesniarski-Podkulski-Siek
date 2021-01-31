ac2576a2cdc317cba59ab7221726a9495d6f4aad
Marcin Wielgos <wielgos.me@gmail.com>
2021-01-31 22:01:47 +0100

    Update payment.controller.js

--- jednostka_rozliczeniowa/api/controllers/payment.controller.js
+++ jednostka_rozliczeniowa/api/controllers/payment.controller.js
@@ -16,108 +16,113 @@ const Payment = models.payment
 
 exports.addPaymentDisposition = (request, result) => {
 
-  const myUser = request.user
+  // const myUser = request.user
 
-  if (request.body.senderAccountnumber == undefined
-    || request.body.recipientAccountnumber == undefined
-    || request.body.paymentTitle == undefined
-    || request.body.currency == undefined
-    || request.body.paymentAmount == undefined) {
+  // if (request.body.senderAccountnumber == undefined
+  //   || request.body.recipientAccountnumber == undefined
+  //   || request.body.paymentTitle == undefined
+  //   || request.body.currency == undefined
+  //   || request.body.paymentAmount == undefined) {
 
-    result.status(400).json({
-      isPaymentAccepted: false,
-      message: "Niepoprawne parametry zapytania"
-    })
-    return
-  }
+  //   result.status(400).json({
+  //     isPaymentAccepted: false,
+  //     message: "Niepoprawne parametry zapytania"
+  //   })
+  //   return
+  // }
 
   
 
-  const senderAccountnumber = validateNumber.validateNumber(request.body.senderAccountnumber)
-  const recipientAccountnumber = validateNumber.validateNumber(request.body.recipientAccountnumber)
-  const paymentTitle = request.body.paymentTitle
-  const paymentAmount = request.body.paymentAmount
-  const currency = request.body.currency
+  // const senderAccountnumber = validateNumber.validateNumber(request.body.senderAccountnumber)
+  // const recipientAccountnumber = validateNumber.validateNumber(request.body.recipientAccountnumber)
+  // const paymentTitle = request.body.paymentTitle
+  // const paymentAmount = request.body.paymentAmount
+  // const currency = request.body.currency
 
-  mongoose.connect(db.url, db.attr)
+  // mongoose.connect(db.url, db.attr)
 
-  let message = "";
-  let flag = false
+  // let message = "";
+  // let flag = false
 
-  if (senderAccountnumber.status != 200) {
-    message += `Błąd w numerze konta nadawcy: ${senderAccountnumber.comment}. `
-    flag = true
-  }
+  // if (senderAccountnumber.status != 200) {
+  //   message += `Błąd w numerze konta nadawcy: ${senderAccountnumber.comment}. `
+  //   flag = true
+  // }
 
-  if (recipientAccountnumber.status != 200) {
-    message += `Błąd w numerze konta odbiorcy: ${recipientAccountnumber.comment}. `
-    flag = true
-  }
+  // if (recipientAccountnumber.status != 200) {
+  //   message += `Błąd w numerze konta odbiorcy: ${recipientAccountnumber.comment}. `
+  //   flag = true
+  // }
 
-  let paymentStatus = "accepted"
+  // let paymentStatus = "accepted"
 
-  if (currency != "PLN") {
-    message += `Nieobsługiwana waluta. `
-    flag = true
-  }
+  // if (currency != "PLN") {
+  //   message += `Nieobsługiwana waluta. `
+  //   flag = true
+  // }
 
-  if (paymentAmount > 1000000) {
-    flag = true
-    message += `Kwota przekracza 1000000 PLN. System obsługuje przelewy poniżej tej kwoty. `
-  }
+  // if (paymentAmount > 1000000) {
+  //   flag = true
+  //   message += `Kwota przekracza 1000000 PLN. System obsługuje przelewy poniżej tej kwoty. `
+  // }
 
-  if (paymentAmount > 1000 && paymentAmount <= 1000000 && flag == false) {
-    paymentStatus = "revision"
-    message += `Kwota przekracza 1000 PLN, przelew może zostać zlecony do zatwierdzenia ręcznego. `
-  }
+  // if (paymentAmount > 1000 && paymentAmount <= 1000000 && flag == false) {
+  //   paymentStatus = "revision"
+  //   message += `Kwota przekracza 1000 PLN, przelew może zostać zlecony do zatwierdzenia ręcznego. `
+  // }
 
 
 
-  if (flag) {
-    result.status(400).json({
-      isPaymentAccepted: false,
-      message: `Przyjęcie przelewu odrzucone. ${message}`
-    })
-  }
+  // if (flag) {
+  //   result.status(400).json({
+  //     isPaymentAccepted: false,
+  //     message: `Przyjęcie przelewu odrzucone. ${message}`
+  //   })
+  // }
 
-  else {
+  // else {
 
-    const senderBankCode = senderAccountnumber.accountnumber.substring(4, 7)
-    const recipientBankCode = recipientAccountnumber.accountnumber.substring(4, 7)
+  //   const senderBankCode = senderAccountnumber.accountnumber.substring(4, 7)
+  //   const recipientBankCode = recipientAccountnumber.accountnumber.substring(4, 7)
 
     
 
-    // if (!auth.checkIfHasAccessToBank(myUser, senderBankCode)) {
-    //   result.status(400).json({
-    //     isPaymentAccepted: false,
-    //     message: `Niewystarczające uprawnienia. Przyjęcie przelewu odrzucone. ${message}`
-    //   })
-    //   return
-    // }
+  //   if (!auth.checkIfHasAccessToBank(myUser, senderBankCode)) {
+  //     result.status(400).json({
+  //       isPaymentAccepted: false,
+  //       message: `Niewystarczające uprawnienia. Przyjęcie przelewu odrzucone. ${message}`
+  //     })
+  //     return
+  //   }
 
 
-    const payment = new Payment({
-      senderAccountnumber: senderAccountnumber.accountnumber,
-      senderBankCode,
-      recipientAccountnumber: recipientAccountnumber.accountnumber,
-      recipientBankCode,
-      paymentTitle: paymentTitle,
-      paymentAmount: paymentAmount,
-      delegatingSession: sessionData.getCurrentSession(),
-      servingSession: sessionData.getCurrentSession(),
-      paymentStatus: paymentStatus
-    })
+  //   const payment = new Payment({
+  //     senderAccountnumber: senderAccountnumber.accountnumber,
+  //     senderBankCode,
+  //     recipientAccountnumber: recipientAccountnumber.accountnumber,
+  //     recipientBankCode,
+  //     paymentTitle: paymentTitle,
+  //     paymentAmount: paymentAmount,
+  //     delegatingSession: sessionData.getCurrentSession(),
+  //     servingSession: sessionData.getCurrentSession(),
+  //     paymentStatus: paymentStatus
+  //   })
 
-    payment.save().then(r => {
-      mongoose.connection.close()
-      result.status(200).json({
-        isPaymentAccepted: true,
-        message: `Zlecenie przelewu zostało przyjęte do realizacji ${message} `,
-        payment
-      })
+  //   payment.save().then(r => {
+  //     mongoose.connection.close()
+  //     result.status(200).json({
+  //       isPaymentAccepted: true,
+  //       message: `Zlecenie przelewu zostało przyjęte do realizacji ${message} `,
+  //       payment
+  //     })
 
-    })
-  }
+  //   })
+  // }
+
+  result.status(200).json({
+          isPaymentAccepted: true,
+          message: `OK `
+        })
 
 }
 
