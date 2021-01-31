d94989103bb0b5e0ece2480fec676c406ed656f8
Marcin Wielgos <wielgos.me@gmail.com>
2021-01-31 19:51:14 +0100

    Update auth.route.js

--- jednostka_rozliczeniowa/api/routes/auth.route.js
+++ jednostka_rozliczeniowa/api/routes/auth.route.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
   const router = require("express").Router()
   const authController = require("../controllers/auth.controller.js")
-  const passport = require("passport-local")
+  const passport = require("passport")
 
   router.post('/login', passport.authenticate('local', {session: false}), authController.login)
   router.post('/register', authController.register)
