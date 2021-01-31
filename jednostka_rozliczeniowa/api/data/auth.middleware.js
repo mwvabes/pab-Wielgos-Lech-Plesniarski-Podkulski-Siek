5a880a21104a4db1e4cd4d60aeb139c7c3ee2409
Marcin Wielgos <wielgos.me@gmail.com>
2021-01-31 18:29:59 +0100

    Update auth.middleware.js

--- jednostka_rozliczeniowa/api/data/auth.middleware.js
+++ jednostka_rozliczeniowa/api/data/auth.middleware.js
@@ -1,4 +1,5 @@
 const passport = require('passport')
+const jwt = require('jsonwebtoken')
 
 
 // exports.auth1 = (request, result, next) => {
@@ -8,6 +9,26 @@ const passport = require('passport')
 const models = require("../models")
 const User = models.user
 
+// exports.auth = (request, response, next) => {
+//     return passport.authenticate('jwt', { session: false })(request, response, next)
+// }
+
 exports.auth = (request, response, next) => {
-    return passport.authenticate('jwt', { session: false })(request, response, next)
+    const token = request.header.authentication;
+
+    // check json web token exists & is verified
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+        if (err) {
+          console.log(err.message);
+          response.redirect('/login');
+        } else {
+          console.log(decodedToken);
+          request.user = decodedToken
+          next();
+        }
+      });
+    } else {
+      response.redirect('/login');
+    }
 }
